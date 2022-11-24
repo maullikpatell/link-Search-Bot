086a684fa530a768e0b00ec52a94c0ae51c616b4
maullikpatell <115573338+maullikpatell@users.noreply.github.com>
2022-11-25 03:04:08 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/AM-ROBOTS/Mdisk-Search-Bot.git /Mdisk-Search-Bot
-cd /Mdisk-Search-Bot
+git clone https://github.com/maullikpatell/link-Search-Bot.git /link-Search-Bot
+cd /link-Search-Bot
 pip3 install -r requirements.txt
 echo "Starting Bot....ANURAG"
 python3 main.py
