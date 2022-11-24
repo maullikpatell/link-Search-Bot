87048284d52fa3362101f1162b05bb158710dc4e
maullikpatell <115573338+maullikpatell@users.noreply.github.com>
2022-11-25 03:05:47 +0530

    Update forcesub.py

--- TeamTeleRoid/forcesub.py
+++ TeamTeleRoid/forcesub.py
@@ -47,7 +47,7 @@ async def ForceSub(bot: Client, cmd: Message):
     except Exception:
         await bot.send_message(
             chat_id=cmd.from_user.id,
-            text="Somthing Went Wrong. Contact My [TGNVS](https://t.me/tgnvs).",
+            text="Somthing Went Wrong. Contact My [TGNVS](https://t.me/dtg_tv).",
             parse_mode="markdown",
             disable_web_page_preview=True
         )
